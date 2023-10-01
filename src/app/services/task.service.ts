c3c41d84667f4e5d0136bdf4dd9993e0b7d7df76
amosn593 <amosndonga@gmail.com>
2023-10-01 22:42:56 +0300

    set up httpcliet , Get and delete methods ready

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { TASKS } from '../mock-tasks';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Task } from '../Task';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:3000/tasks';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  getTask() : Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks
+  getTask(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
   }
 }
